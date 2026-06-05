4bf0125974c96bedd7bbe000a746ba9c6971be27
aaronflores92 <dsp.hb.aaronflores@gmail.com>
2026-06-05 16:53:19 -0700

    update: login page converted to a web form that accepts a username and passord via a button click

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+
 function LoginPage() {
-    return <h1>Login</h1>;
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('Username:', username);
+        console.log('Password:', password);
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Login!</button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
